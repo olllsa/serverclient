07c1e150f7ca839da5b727601c68f26c1efc9974
olllsa <128024225+olllsa@users.noreply.github.com>
2023-03-16 15:20:05 +0700

    Add comments for list.h

--- server/list.h
+++ server/list.h
@@ -1,9 +1,56 @@
+/*********************************************************************
+This module implements a minimal set of function for working with list
+********************************************************************/
+
+// Structure of list node
 typedef struct Node {
     unsigned int value;
     struct Node *next;
 } Node;
 
-
+/*********************************************************************
+ *
+ * @purpose  Push element data to list h
+ *
+ * @param Node **h              head of list
+ *
+ * @param unsigned int data     data
+ *
+ * @return void
+ *
+ * @note This function push element "data" to list "h"
+ *
+ * @end
+ *
+ *********************************************************************/
 void push(Node **h, unsigned int data);
-int pop(Node **h);
+
+/*********************************************************************
+ *
+ * @purpose  Pop element from list h
+ *
+ * @param Node **h              head of list
+ *
+ * @return unsigned int         data of element
+ *
+ * @note This function pop element from list "h"
+ *
+ * @end
+ *
+ *********************************************************************/
+unsigned int pop(Node **h);
+
+/*********************************************************************
+ *
+ * @purpose  Delete List and free memory
+ *
+ * @param Node **h              head of list
+ *
+ * @return void
+ *
+ * @note This function deletes list "h" and free memory
+ *
+ * @end
+ *
+ *********************************************************************/
 void deleteList(Node **head);
